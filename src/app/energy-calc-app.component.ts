6a77d62dec118b3763a365659a6c69f26fc340fe
cchia <casperchiakyechuen@gmail.com>
2016-05-14 17:27:30 +1000

    Add initial dropdown component

--- src/app/energy-calc-app.component.ts
+++ src/app/energy-calc-app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import {ApplianceDropdownComponent} from "./shared/input/appliance-dropdown/appliance-dropdown.component";
 
 @Component({
   moduleId: module.id,
   selector: 'energy-calc-app-app',
   templateUrl: 'energy-calc-app.component.html',
-  styleUrls: ['energy-calc-app.component.css']
+  styleUrls: ['energy-calc-app.component.css'],
+  directives: [ApplianceDropdownComponent]
 })
 export class EnergyCalcAppAppComponent {
   title = 'energy-calc-app works!';
