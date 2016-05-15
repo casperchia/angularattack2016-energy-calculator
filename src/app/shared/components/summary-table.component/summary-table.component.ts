6aefb0ceb9f164b8d714a84019359f56945b5eef
Anusri Sugathan <Anusri.Sugathan@objectconsulting.com.au>
2016-05-15 15:48:22 +1000

    Added calculateTotal function.

--- src/app/shared/components/summary-table.component/summary-table.component.ts
+++ src/app/shared/components/summary-table.component/summary-table.component.ts
@@ -13,8 +13,21 @@ import {CostCalculatorPipe} from "../../../shared/input/pipes/CostCalculatorPipe
 
 export class SummaryTableComponent implements OnInit{
     @Input() appliances: Appliance[];
+    dailyCostTotal: number;
+    monthlyCostTotal:number;
+    quarterlyCostTotal:number;
+    yearlyCostTotal:number;
 
     ngOnInit(){
+    }
+
+    calculateTotal(appliances:Appliance[]){
+      for(let i=0; i < appliances.length; i++){
+        this.dailyCostTotal = this.dailyCostTotal + this.appliances[i].cost;
+        this.monthlyCostTotal = this.monthlyCostTotal + (this.appliances[i].cost *30);
+        this.quarterlyCostTotal = this.quarterlyCostTotal + (this.appliances[i].cost * 90);
+        this.yearlyCostTotal = this.yearlyCostTotal + (this.appliances[i].cost * 360);
+      }
     }
 
     get diagnostic() { return JSON.stringify(this.appliances, null, 4); }
