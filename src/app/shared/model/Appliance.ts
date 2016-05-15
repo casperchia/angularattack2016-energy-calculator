8b01b5cf31f07b69d420d0e23d87ca9ff4b6e137
cchia <casperchiakyechuen@gmail.com>
2016-05-15 13:57:07 +1000

    Remove unused properties in Appliance model

--- src/app/shared/model/Appliance.ts
+++ src/app/shared/model/Appliance.ts
@@ -5,9 +5,5 @@ export class Appliance{
   name: string;
   power: Power = new Power();
   duration: Time = new Time();
-  cost: number;
-  costDaily: number;
-  costMonthly: number;
-  costQuarterly: number;
-  costYearly: number;
+  cost: number = 0;
 }
